9cd7a413ec0406b34d58a438ce42d17ff56f1e99
Vibhab-T <vibhabtimsina86@gmail.com>
2025-01-04 11:16:20 +0545

    added a basic response as well. Responds with HTML

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers); //printing request datas to the console
+  res.setHeader("Content-Type", "text/html"); //setting a response header
+  res.write(
+    "<html><head><title>My First Page</title></head><body><h1>Hello Hello From The Server!</h1></body></html>" //writing a response
+  );
+  res.end(); //ending a response
 });
 
 server.listen(3000);
